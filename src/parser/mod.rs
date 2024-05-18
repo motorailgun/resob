696acc36faf44002f510359270b671ea1c928fa1
motorailgun <28751910+motorailgun@users.noreply.github.com>
2024-05-19 05:53:04 +0900

    chore: code cleanup pt. 3

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,3 +1,2 @@
-pub mod instructions;
 pub mod module;
 pub mod sections;
--- src/parser/sections/mod.rs
+++ src/parser/sections/mod.rs
@@ -1,5 +1,6 @@
 pub mod code_section;
 pub mod function_section;
+pub mod instructions;
 pub mod type_section;
 pub mod types;
 
